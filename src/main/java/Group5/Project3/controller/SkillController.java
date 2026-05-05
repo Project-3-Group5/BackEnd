461ce1f213cbcb5cf31224c9cc3ef1a8b8f3faa6
EstrellaOrtiz-F <eortiz-felix@csumb.edu>
2026-05-04 20:39:10 -0700

    Changed int id to long.

--- src/main/java/Group5/Project3/controller/SkillController.java
+++ src/main/java/Group5/Project3/controller/SkillController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/skill")
+@RequestMapping("/skills")
 public class SkillController {
     private SkillService skillService;
 
